90e142bf1fbbd20be2c6dfc1e5a85660f7cc4b47
hopwesley <ws@sinoc.io>
2020-11-09 16:36:53 +0800

    init

---
+++ phone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  phone
+//
+//  Created by wesley on 2020/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
